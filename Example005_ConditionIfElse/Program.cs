8a32fc0b93cbdfe1ce86570c04ee4bfbce7abbb2
Ibragim.Ast <ibragim.ast@gmail.com>
2022-08-05 15:34:22 +0300

    Add Hello Username

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ibragim")
+{
+    Console.WriteLine("Assalamu Aleykum, Ibragim");
+}
+else
+{
+    Console.Write("Марша вог1ийла, ");
+    Console.WriteLine(username);
+}
